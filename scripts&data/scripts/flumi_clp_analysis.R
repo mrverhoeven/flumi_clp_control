ea1138f7106a691b4a8afc44932b5eed874098bd
Michael Verhoeven <Michael.Verhoeven@state.mn.us>
2025-06-02 15:16:15 -0500

    adds rough figs for clp outcomes (ignoring underlying data structure)

--- scripts&data/scripts/flumi_clp_analysis.R
+++ scripts&data/scripts/flumi_clp_analysis.R
@@ -109,6 +109,72 @@ allobs[sample_type_descr == "sampled" & species_code == "PC" & abundance > 0,
          "mean_ra_clp" = mean(abundance)) ,
        .(site,survey_purpose, lake_name, survey_date, k_survey_id) ]
 
+#combine
+surveydat[clp_stats, on = .(k_survey_id) , c("n_clp", "mean_ra_clp")   := .(n_clp, mean_ra_clp) ]
+
+#calc clp frequency
+surveydat[ , freq := n_clp/n_samp , ]
+
+#set factor order
+surveydat[ , survey_purpose := factor(survey_purpose, levels = c("Pre", "Post", "1YAT")) , ]
+
+
+#why are we getting NA values? (because these survey data )
+allobs[k_survey_id %in% c(1,2) , .N , .(lake_name, survey_date, species, abundance) ]
+
+
+ggplot(surveydat, aes(site, freq))+
+  geom_violin()+
+  geom_boxplot()+
+  facet_wrap(~survey_purpose, nrow=1)
+
+
+ggplot(surveydat, aes(survey_purpose, freq))+
+  geom_violin()+
+  geom_boxplot()+
+  facet_wrap(~site, ncol=1)
+
+
+ggplot(surveydat, aes(survey_purpose, freq))+
+  geom_path( aes(group = lake_name))+
+  facet_wrap(~site, ncol=1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
